5ea4ca8f79565ca34c972604d50eeaccf9973115
Utkarsha <ut.medhekar2003@gmail.com>
2022-10-30 19:46:36 +0530

    Initial commit

---
+++ tut3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//this cprogram was created by me
+/*this 
+is 
+a
+ multi 
+line 
+comment*/
+int main(){
+    int sum = 6;
+    cout<<"hello world"<< sum;
+    return 0;
+}
---
+++ tut4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+    }
+int main(){
+
+    // int a = 4;
+    // int b = 5;
+    // int a = 14;
+    // int b = 15;
+    // float pi = 3.14;
+    // char c ='u';
+    bool p = false;
+    sum();
+   cout<<glo<< p;
+    // cout<<"This is tutorial 4. \nHere the value of a is "<<a<<". \nThe value of b is "<<b;
+    // cout<<". \nThe value of pi is = "<<pi<<".";
+    // cout<<" \nThe value of c is = "<<c<<".";
+    return 0;
+}
