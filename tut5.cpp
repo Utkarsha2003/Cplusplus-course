4e7180446b9a38bfd72838986c920b3574e5e50d
Utkarsha Medhekar <113205154+Utkarsha2003@users.noreply.github.com>
2023-03-27 00:57:34 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num1, num2;
+    cout<<"enter the value of num1: \n"; /* '<<' this is called insertion operator*/
+    cin>>num1; /* '>>' is called extraction operator */
+    
+ cout<<"enter the value of num2: \n"; /* '<<' this is called insertion operator*/
+    cin>>num2; /* '>>' is called extraction operator */
+
+cout<<"The sum is "<< num1+num2;
+
+    return 0;
+}
